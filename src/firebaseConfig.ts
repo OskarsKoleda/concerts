4009d2c9f184e2ef4a830e13ecb60d1fae222770
oskars <oskarskoleda@gmail.com>
2024-06-18 10:20:30 +0300

    Configurations moved to gitignore

--- src/firebaseConfig.ts
+++ src/firebaseConfig.ts
@@ -1,20 +1,10 @@
 import { initializeApp, getApps } from "firebase/app";
 import { getDatabase } from "firebase/database";
 import "firebase/database";
-
-const firebaseConfig = {
-  apiKey: "AIzaSyAWp6EczcIvriXy-Oq6NNAYdCHKc8X8fUk",
-  authDomain: "metal-concerts.firebaseapp.com",
-  databaseURL:
-    "https://metal-concerts-default-rtdb.europe-west1.firebasedatabase.app",
-  projectId: "metal-concerts",
-  storageBucket: "metal-concerts.appspot.com",
-  messagingSenderId: "647561205187",
-  appId: "1:647561205187:web:04e593b6ed5c48e75d1976",
-};
+import conf from "./configurations";
 
 if (!getApps().length) {
-  initializeApp(firebaseConfig);
+  initializeApp(conf);
 }
 const db = getDatabase();
 
